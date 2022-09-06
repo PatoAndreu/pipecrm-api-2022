0573750d8f784a07771bc3cbd50ef86e6b54a34a
Patricio Andreu <patricioandreu@gmail.com>
2022-09-06 11:09:00 -0400

    Add casts

--- app/Models/PipelineStage.php
+++ app/Models/PipelineStage.php
@@ -10,14 +10,15 @@ class PipelineStage extends Model
   use HasFactory;
 
   protected $fillable = ['name', 'probability_of_close', 'pipeline_id'];
+	protected $casts = ['probability_of_close' => 'integer','pipeline_id' => 'integer'];
 
-  public function Pipeline()
-  {
+  public function Pipeline(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+	{
     return $this->belongsTo(Pipeline::class);
   }
 
-  public function Deals()
-  {
+  public function Deals(): \Illuminate\Database\Eloquent\Relations\HasMany
+	{
     return $this->hasMany(Deal::class);
   }
 }
