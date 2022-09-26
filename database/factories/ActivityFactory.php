b958603ae189c22036e0eadd7c3966b6ac079cd6
Patricio Andreu <patricioandreu@gmail.com>
2022-09-26 11:07:54 -0300

    ActivityFactory changes

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -18,11 +18,13 @@ class ActivityFactory extends Factory
 	public function definition(): array
 	{
 		return [
-			"text"       => $this->faker->sentence(),
-			"pinned"     => $this->faker->boolean(),
-			"completed"  => $this->faker->boolean(),
-			"date"       => $this->faker->dateTimeBetween('-1 month', '+1 month'),
-			"time"       => $this->faker->dateTimeBetween('-1 month', '+1 month'),
+			"text"       => $this->faker->realText(50),
+			"pinned"     => $this->faker->boolean(33),
+			"completed"  => $this->faker->boolean(33),
+			"date" => $this->faker->dateTimeThisMonth(),
+			"time" => $this->faker->dateTimeBetween('-1 month', '+1 month'),
+//			"date"       => $this->faker->dateTimeBetween('-1 month', '+1 month'),
+//			"time"       => $this->faker->dateTimeBetween('-1 month', '+1 month'),
 			"type"       => $this->faker->randomElement(['note', 'call', 'email', 'meeting']),
 			"owner_id"   => User::all()->random()->id,
 			"contact_id" => Contact::all()->random()->id,
